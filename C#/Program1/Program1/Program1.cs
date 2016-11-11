442cd2cb9f9b13e05ee74e42739fd73bfae481e7
Matt-KozLowskiB <matt.b.kozlowski@gmail.com>
2016-11-11 14:50:49 -0800

    simple C # program added
    program simply outputs text to console

---
+++ C#/Program1/Program1/Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+#region Closet methods
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name, please:");
+            String Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+            Console.WriteLine("Press Return to terminate....");
+            Console.Read();
+
+
+
+
+        }
+    }
+}
+#endregion Closet methods
