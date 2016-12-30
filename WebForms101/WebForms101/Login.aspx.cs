0459d6a14f6308013cf6eb356059339f4f5b98b1
matt-kozlowskib <matt.b.kozlowski@gmail.com>
2016-12-29 19:57:58 -0800

    web form project
    Basic Login Functionality using ASP.NET

---
+++ WebForms101/WebForms101/Login.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms101
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click1(object sender, EventArgs e)
+        {
+            lblName.Text = txtLast.Text + ", " + txtFirst.Text;
+        }
+
+
+    }
+}
